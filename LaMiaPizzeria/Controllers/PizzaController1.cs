e57ee6974e7baa4750b6a4522645c8ae002882fe
Gabriele31201 <gabri31201@gmail.com>
2023-05-17 17:09:49 +0200

    add new folder Pizza and new index

--- LaMiaPizzeria/Controllers/PizzaController1.cs
+++ LaMiaPizzeria/Controllers/PizzaController1.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LaMiaPizzeria.Database;
+using Microsoft.AspNetCore.Mvc;
+using LaMiaPizzeria.Models;
 
 namespace LaMiaPizzeria.Controllers
 {
     public class PizzaController1 : Controller
     {
-        public IActionResult Index()
+        public IActionResult NewIndex()
         {
-            return View();
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<PizzaModel> pizze = db.Pizzas.ToList();
+           
+                return View(pizze);
+            }
+                
         }
     }
 }
