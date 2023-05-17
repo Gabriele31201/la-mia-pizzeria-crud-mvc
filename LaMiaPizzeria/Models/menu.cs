5f174b9ba2a9229731eae7f9d8d0c30a496de346
Gabriele31201 <gabri31201@gmail.com>
2023-05-17 12:49:48 +0200

    Aggiungere i file di progetto.

---
+++ LaMiaPizzeria/Models/menu.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace LaMiaPizzeria.Models
+{
+    [Key]
+    public class menu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ingredienti { get; set; }
+
+        public string immagine { get; set; }
+
+        public float prezzo { get; set; }
+
+
+        public menu
+    }
+}
